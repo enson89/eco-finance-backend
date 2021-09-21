9fbebbe27c5e2a7e92a4423956ea130da56a49bd
Enson Yeoh <enson.yeoh@aquariux.com>
2021-09-21 15:05:49 +0800

    removed used code and files

--- src/docs/components.js
+++ src/docs/components.js
@@ -15,42 +15,6 @@ module.exports = {
                         example:"abc123"
                     }
                 }
-            },
-            id:{
-                type:'string',
-                description:"An id of a todo",
-                example: "tyVgf"
-            },
-            Todo:{
-                type:'object',
-                properties:{
-                    id:{
-                        type:'string',
-                        description:"Todo identification number",
-                        example:"ytyVgh"
-                    },
-                    title:{
-                        type:'string',
-                        description:"Todo's title",
-                        example:"Coding in JavaScript"
-                    },
-                    completed:{
-                        type:"boolean",
-                        description:"The status of the todo",
-                        example:false
-                    }
-                }
-            },            
-            Error:{
-                type:'object',
-                properties:{
-                    message:{
-                        type:'string'
-                    },
-                    internal_code:{
-                        type:'string'
-                    }
-                }
             }
         }
     }
--- src/docs/routes/apis.js
+++ src/docs/routes/apis.js
@@ -1,23 +1,9 @@
 const login = require('./login');
-const getTodos = require('./get-todos');
-const getTodo = require('./get-todo');
-const createTodo = require('./create-todo');
-const updateTodo = require('./update-todo');
-const deleteTodo = require('./delete-todo');
 
 module.exports = {
     paths:{
         '/login': {
             ...login
-        },
-        '/todos':{
-            ...getTodos,
-            ...createTodo
-        },
-        '/todos/{id}':{
-            ...getTodo,
-            ...updateTodo,
-            ...deleteTodo
         }
     }
 }
